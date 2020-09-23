921cd69712506d64fa4e82a90752fe97014fd1bc
bigintjin <jininja7@gmail.com>
2020-09-23 09:05:17 -0700

    new endpoints to get and post to mongo

--- server/api/index.js
+++ server/api/index.js
@@ -29,4 +29,25 @@ router.post("/weather",  async (req, res) => {
     res.send(JSON.stringify(weatherData, null, 4));
 });
 
+// POST Request - get the weather data from the api, save it to mongo, then return the data back
+router.post("/weatherMongo", async(req, res) => {
+    const {zipCode, tempMetric} = req.body;
+    let weather = new Weather();
+    let weatherData = await weather.getWeatherData(zipCode, tempMetric);
+
+    await weather.saveWeatherDataToMongo(zipCode, weatherData);
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(weatherData, null, 4));
+})
+
+// GET Request - get the weather data saved from Mongo
+router.get("/weatherMongo", async(req, res) => {
+    const {zipCode} = req.query;
+    let weather = new Weather();
+
+    let weatherData = await weather.getWeatherDataFromMongo(zipCode);
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(weatherData, null, 4));
+})
+
 module.exports = router;
