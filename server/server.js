e8a66ff76a9706c1653a5c7e0b2fa44970eba263
bigintjin <jininja7@gmail.com>
2020-09-23 10:03:39 -0700

    update path to find the build folder

--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@ app.use("/api", apis);
 // If in production, then use static frontend build files.
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 }
 
