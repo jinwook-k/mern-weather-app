af8b5e244c0b1aaf4cfc0182e3b87722b86ce2ae
bigintjin <jininja7@gmail.com>
2020-09-24 07:30:27 -0700

    add path

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
