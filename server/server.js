ea4ab4ea88c496ec8659a60b0c35f9527cab08b9
bigintjin <jininja7@gmail.com>
2020-09-25 06:01:17 -0700

    remove extra path var

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const mongoose = require('mongoose');
 
 const path = require('path');
